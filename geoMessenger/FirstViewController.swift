0b5d3b8826626c8381820eea05845a379def9845
8605lux <31935899+8605lux@users.noreply.github.com>
2017-10-13 23:02:42 -0500

    Initial Commit

---
+++ geoMessenger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  geoMessenger
+//
+//  Created by Tom on 13/10/2017.
+//  Copyright © 2017 Deitel and Associates , Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ geoMessenger/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  geoMessenger
+//
+//  Created by Tom on 13/10/2017.
+//  Copyright © 2017 Deitel and Associates , Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
